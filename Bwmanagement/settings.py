a84cd63c606c7e5af50dec4dec82c9247f7d28df
Sizan Mahmud <59968225+sizan378@users.noreply.github.com>
2021-10-11 22:40:49 +0600

    Add files via upload

--- Bwmanagement/settings.py
+++ Bwmanagement/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0h4k6r+b+pd%*n@eixv@3&1b$2ge13$+y^onlif5o#6)%8!=h2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Bwmanagement.wsgi --log-file -
