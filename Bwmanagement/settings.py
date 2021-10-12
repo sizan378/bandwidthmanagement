b39cfa8fbc6263ac1f1301a9532b9da0fe0b0b24
Sizan Mahmud <59968225+sizan378@users.noreply.github.com>
2021-10-12 22:39:49 +0600

    Update settings.py

--- Bwmanagement/settings.py
+++ Bwmanagement/settings.py
@@ -121,12 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = os.path.join(BASE_DIR,"static/")
+STATIC_ROOT = os.path.join(BASE_DIR,"static/")
 MEDIA_URL = '/media/'
-MEDIA_URL = os.path.join(BASE_DIR,"media/")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
